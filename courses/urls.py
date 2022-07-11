040a1268a1d9c242422ee114e62c3280c2766c37
AryaNazari86 <nazariarya86@gmail.com>
2022-07-11 12:11:06 +0430

    search and order added

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from courses.views import search, home
 urlpatterns = [
-    path('search/', search),
-    path('', home),
+    path('search/', search, name='search'),
+    path('', home, name='home'),
 ]
