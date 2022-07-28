2324415aed6a061dcb3406660a83b14490e6c861
Mohammad Hajikandi <73636328+mohammad488@users.noreply.github.com>
2022-07-28 05:50:43 +0000

    @AryaNazari86 Course Details page added to django

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from courses.views import search, home, CourseList, SourceDel
+from courses.views import search, home, course_details, CourseList, SourceDel
 urlpatterns = [
     path('search/', search, name='search'),
     path('', home, name='home'),
+    path('course/', course_details, name='course'),
     path('add/', CourseList.as_view()),
     path('del/', SourceDel)
 ]
