b4b38dd48ad498a092a3f54db432f8a1263b7a08
mohammad488 <73636328+mohammad488@users.noreply.github.com>
2022-08-15 13:31:18 +0430

    Search pagination added

--- courses/views.py
+++ courses/views.py
@@ -49,7 +49,17 @@ def search(request):
         query.filter(rating__lte=request.GET['min-rating'],
                      rating__gte=request.GET['max-rating'])'''
 
-    return render(request, 'searchResult.html', {'courses': query, 'searchName': request.GET.get('name') or '', 'searchOrder': request.GET.get('order') or 'price', 'searchMaxPrice': request.GET.get('max-price') or '2500000'})
+    # Pagination
+    take = 20
+    pagesCount = query.count() // take
+    if request.GET.get('page') == None or int(request.GET.get('page')) < 0 or int(request.GET.get('page')) > query.count() // take:
+        page = 0
+    else:
+        page = int(request.GET.get('page'))
+    skip = int(page) * take
+    query = query[skip:skip + take]
+
+    return render(request, 'searchResult.html', {'courses': query, 'searchName': request.GET.get('name') or '', 'searchOrder': request.GET.get('order') or 'price', 'searchMaxPrice': request.GET.get('max-price') or '2500000', 'currentPage': request.GET.get('page'), 'pages': range(pagesCount + 1)})
 
 
 def course_details(request, id):
