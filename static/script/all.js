bb776da1a80627308e4dcfacddb3856c7fd8351d
mohammad488 <73636328+mohammad488@users.noreply.github.com>
2022-09-02 15:28:24 +0430

    Some problems fixed befor deployment

--- static/script/all.js
+++ static/script/all.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    $("#loaderContainer").fadeOut();
-    animation.stop();
-});
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     $("#loaderContainer").fadeOut();
+//     animation.stop();
+// });
